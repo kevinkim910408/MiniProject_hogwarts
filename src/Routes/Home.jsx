afe1eee208981c623a1afcb1cd43a39ebf5463a2
kordobby <poordobby@naver.com>
2022-05-23 23:44:26 +0900

    help me

--- src/Routes/Home.jsx
+++ src/Routes/Home.jsx
@@ -3,7 +3,9 @@ import styled from "styled-components";
 
 // Import Header Menu Components
 import HeaderNavi from "../Components/HeaderNavi.jsx";
-import TestImg from '../img/background.png';
+import Video from '../video/HarryPotter.mp4';
+
+
 
 const Home = () => {
     return (
@@ -13,7 +15,9 @@ const Home = () => {
 
             {/* background img => 동영상으로 바꿀 예정 */}
             <HomeMain>
-            <img src={TestImg} alt = "배경"></img>
+                <BackVideo controls loop>
+                    <source src={Video} type="video/mp4"></source>
+                </BackVideo>
             </HomeMain>
         </>
     )
@@ -22,6 +26,39 @@ const Home = () => {
 export default Home;
 
 const HomeMain = styled.div`
-  width : 100%;
+  padding: 0;
+  width: 100vw;
   height: 100vh;
+  overflow: hidden;
+  position: absolute;
+`;
+
+const BackVideo = styled.video`
+    width: 100vw;
+    position: absolute;
+    @media screen and (max-aspect-ratio: 1219/685) {
+       width: 100%;
+       height: 100vh;
+    }
+    &::-webkit-media-controls-fullscreen-button {
+        display: none !important;
+    };
+    &::-webkit-media-controls-play-button {
+        display: none !important;
+    };
+    &::-webkit-media-controls-current-time-display {
+        display: none !important;
+    };
+    &::-webkit-media-controls-timeline {
+        display: none !important;
+    };
+    &::-webkit-media-controls-time-remaining-display {
+        display: none !important;
+    };
+    &::-webkit-media-controls-mute-button {
+        display: none !important;
+    };
+    &::-webkit-media-controls-volume-slider {
+        display: none !important;
+    };
 `;
