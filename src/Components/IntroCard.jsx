60da6596fe1c9dc1ff9179cc96e75a086b46297e
kvv(Kevin Junho Kim) <59503331+kevinkim910408@users.noreply.github.com>
2022-05-23 02:49:10 -0400

    PAGINATION 아이콘, 기초 CSS
    PAGINATION 아이콘, 기초 CSS

--- src/Components/IntroCard.jsx
+++ src/Components/IntroCard.jsx
@@ -3,13 +3,15 @@
 /* Import */
 import styled from "styled-components";
 
+import CardBG from '../img/CardBG.png'
+
 const IntroCard = ({img, name, date}) => {
 
     return (
         <>
             <Card>
                 <ImageWrap>
-                    <img style={{width:'500px', margin:"50px auto"}} src={img} alt="" />
+                    <img style={{width:'400px',height:'550px', margin:"50px auto", borderRadius:"30px"}} src={img} alt="" />
                 </ImageWrap>
                 <TextWrap>
                     <div>{name}</div>
@@ -27,20 +29,23 @@ export default IntroCard;
 const Card = styled.div`
   max-width: 1600px;
   width: 90%;
-  min-height: 600px;
-  background-color: var(--purple);
-  margin: 30px 0;
+  min-height: 800px;
+  background-image: url(${CardBG});;
+  background-size: cover;
+  margin: 10px 0;
   display: flex;
   flex-direction: row;
-  justify-content: space-evenly;
+  justify-content: center;
   align-items: center;
 `;
 
 const ImageWrap = styled.div`  
+    margin: 50px;
 `;
 
 const TextWrap = styled.div`
-    font-size: 40px;
+    font-size: 30px;
+    color: var(--black);
     @media screen and (max-width: 900px) {
     display: none;
     }
