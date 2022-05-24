fa48a557a45b79c677275a105db944dc388bbdfb
kordobby <poordobby@naver.com>
2022-05-24 09:09:20 +0900

    -

--- src/Routes/Home.jsx
+++ src/Routes/Home.jsx
@@ -5,8 +5,6 @@ import styled from "styled-components";
 import HeaderNavi from "../Components/HeaderNavi.jsx";
 import Video from '../video/HarryPotter.mp4';
 
-
-
 const Home = () => {
     return (
         <>
