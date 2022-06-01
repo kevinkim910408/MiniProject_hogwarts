9382612c1b5fd0d7f2982e55f35f08569bcdb96a
kordobby <poordobby@naver.com>
2022-06-01 20:58:20 +0900

    wordArr

--- src/Components/Words.jsx
+++ src/Components/Words.jsx
@@ -23,7 +23,7 @@ export const generateWordSet = async () => {
     // result에 넣어줌
     .then((result) => {
       // 단어가 한줄에 하나씩있어서 split으로 잘라서 보관 - 단어가 \r\n가 붙어나오던데, 엔터가 \r\n 라고 한다.
-      const wordArr = result.split("\r\n");
+      const wordArr = result.split("\n");
       // 정답단어는 목록에서 랜덤으로 굴려서 생성
       todaysWord = wordArr[Math.floor(Math.random() * wordArr.length)];
       // 단어목록에 중복단어 제거해서 넣어줌
