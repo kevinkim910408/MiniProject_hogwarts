c05334629a599de40796ac7834605b630e98df1a
kvv(Kevin Junho Kim) <59503331+kevinkim910408@users.noreply.github.com>
2022-06-08 05:59:42 -0400

    Before publish
    Before publish

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 // Global CSS
 import GlobalStyled from "./Components/GlobalStyled/GlobalStyled.jsx";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <BrowserRouter>
+    <HashRouter>
         {/* Global CSS */}
         <GlobalStyled />
         <App />
-    </BrowserRouter>
+    </HashRouter>
 );
